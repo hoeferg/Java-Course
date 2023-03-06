58a1df027c556982d6172da1a0f5bdf67711b89c
Gayle Hoefer <gaylehoefer@gmail.com>
2023-03-05 18:40:12 -0700

    First video for java intro is completed

---
+++ FirstJavaProgram.java
@@ -0,0 +1,26 @@
+public class FirstJavaProgram {
+     public static void main (String[] args) {
+//         print will not give space
+//         System.out.print("I love pizza");
+////         println will give the information on the entire line
+//         System.out.println("It's really good!");
+
+//         System.out.print("I love pizza\n");
+//         System.out.print("It's really good!");
+////         \n means add a new line afterwards
+
+//         System.out.println("I love pizza\n");
+//         System.out.println("It's really good!");
+//         This will return the whole first line, then skip a line, then the whole last line
+
+//         System.out.println("\t I love pizza");
+//         System.out.println("It's really good!");
+//         \t will give a tab at the beginning
+
+//         how to literalluy put the phrase in quates. We have to use \ as an escape
+         System.out.println("\"I love pizza\"");
+//         This how to display a backslash
+         System.out.println("\\It's really good!");
+//         type sout and entre to get System.out.println
+     }
+}
