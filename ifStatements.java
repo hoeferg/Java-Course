d742b2bb0826a74015be4e91832976567ea56d29
Gayle Hoefer <gaylehoefer@gmail.com>
2023-03-10 13:04:19 -0700

    Went over if statements

---
+++ ifStatements.java
@@ -0,0 +1,17 @@
+public class ifStatements {
+    public static void main (String[] args){
+//        if statement = performs a block of code if its condition evaluates to be true
+        int age = 13;
+
+        if(age==75) {
+            System.out.println("Okay boomer!");
+        }
+        else if(age>=18) {
+            System.out.println("You are an adult!");
+        } else if (age>=13) {
+            System.out.println("You are a teenager");
+        } else {
+            System.out.println("You are not an adult");
+        }
+    }
+}
