b87130bccf879e1eb52ede70be7ace1f6509375a
Gayle Hoefer <gaylehoefer@gmail.com>
2023-03-10 12:20:20 -0700

    Went over expressions

---
+++ Expressions.java
@@ -0,0 +1,14 @@
+public class Expressions {
+    public static void main(String[] args) {
+//        expression = operands & operators
+//        operands = values, variables, numbers, quantity
+//        operators = + - * / %
+        double friends = 10;
+
+        friends = (double) friends / 3;
+//        When we deveide the number will be rounded. you have to use a double to save the decimal
+        friends--;
+
+        System.out.println(friends);
+    }
+}
