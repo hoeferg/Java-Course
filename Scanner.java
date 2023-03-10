cf459669607216eba75ab995c8b09c7fb8a8c655
Gayle Hoefer <gaylehoefer@gmail.com>
2023-03-10 13:59:40 -0700

    Went over if statements

--- Scanner.java
+++ Scanner.java
@@ -1,17 +1,17 @@
-public class Scanner {
-    public static void main(String[] args) {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
-        System.out.println("How old are you? ");
-        int age = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("What is your favoirte food?");
-        String food = scanner.nextLine();
-
-        System.out.println("Hello "+name);
-        System.out.println("You are "+age+" years old");
-        System.out.println("You like "+food);
-    }
-}
+//public class Scanner {
+//    public static void main(String[] args) {
+//        java.util.Scanner scanner = new java.util.Scanner(System.in);
+//
+//        System.out.println("What is your name?");
+//        String name = scanner.nextLine();
+//        System.out.println("How old are you? ");
+//        int age = scanner.nextInt();
+//        scanner.nextLine();
+//        System.out.println("What is your favoirte food?");
+//        String food = scanner.nextLine();
+//
+//        System.out.println("Hello "+name);
+//        System.out.println("You are "+age+" years old");
+//        System.out.println("You like "+food);
+//    }
+//}
